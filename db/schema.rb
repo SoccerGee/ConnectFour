ab6e18299beae87fdd6ccf550678b65b20427f17
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-12 22:52:25 -0400

    added player type fk relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013024835) do
+ActiveRecord::Schema.define(version: 20161013025016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,4 +48,5 @@ ActiveRecord::Schema.define(version: 20161013024835) do
   add_foreign_key "games", "players", column: "red_player_id"
   add_foreign_key "moves", "games"
   add_foreign_key "moves", "players"
+  add_foreign_key "players", "player_types", column: "type"
 end

d60c2543bc5b804037b9e74a2959bd4d54e894f6
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-12 23:47:45 -0400

    add unique index on player email

---
+++ db/migrate/20161013034540_unique_index_on_player_email.rb
@@ -0,0 +1,5 @@
+class UniqueIndexOnPlayerEmail < ActiveRecord::Migration[5.0]
+  def change
+    add_index :players, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013033707) do
+ActiveRecord::Schema.define(version: 20161013034540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20161013033707) do
     t.integer  "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_players_on_email", unique: true, using: :btree
   end
 
   add_foreign_key "games", "players", column: "winner_id"

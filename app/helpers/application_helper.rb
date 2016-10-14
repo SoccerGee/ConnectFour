62ef35b68e0cd9cf576e34f57f761497ecfa547c
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-14 15:35:38 -0400

    sorted out the beginnings of the nav bar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
-
-  #TODO: move the hash values to environment configs.
-  def cpu_attributes_hash
-    {
-      email: "connectfour@salesloft.com",
-        pin: "1337"
-    }
+  # nav bar abstraction to help with variable authentication states
+  def nav_bar
+    render partial: "/partials/nav/nav_bar"
   end
+
+  def nav_bar_menus
+    partial = user_signed_in? ? "logged_in" : "logged_out"
+    render partial: "/partials/nav/#{partial}"
+  end
+
 end

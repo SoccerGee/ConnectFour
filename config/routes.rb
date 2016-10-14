48b0fda86727a9f9fedae9aa2b669b5efaf05d48
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-14 13:40:35 -0400

    removing other pre refactor artifacts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'player#index'
-
-  resources :player, only: [:create, :new]
-
-  get 'sign_in' => 'player#new_session', :as => :new_player_session
-  post 'sign_in' => 'player#create_session', :as => :create_player_session
-  delete 'sign_out' => 'player#sign_out', :as => :destroy_player_session
-
-
 end

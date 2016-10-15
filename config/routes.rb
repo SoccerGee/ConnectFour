6e36f2c84f41d40b435abec67a18d5ec4780353d
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-15 18:33:07 -0400

    getting ready for moves

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, except: [:index, :create, :new, :edit, :show, :update, :destroy] do
-    # nest only :create
     # don't cheat!!  no edit for you!!
     # games will never be created with a form.  They simply begin with users (cpu and user).
-    resources :games, shallow: true, except: [:edit, :new]
+    resources :games, shallow: true, except: [:edit, :new] do
+      resources :moves, shallow:true
+    end
   end
 
   root "home#index"

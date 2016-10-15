84d212e71eb32f8b413c3e8305254fc2581eec48
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-15 13:46:38 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015173334) do
+ActiveRecord::Schema.define(version: 20161015174242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20161015173334) do
   create_table "moves", force: :cascade do |t|
     t.integer  "x_loc"
     t.integer  "y_loc"
-    t.integer  "player_id"
+    t.integer  "user_id"
     t.integer  "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_moves_on_game_id", using: :btree
-    t.index ["player_id"], name: "index_moves_on_player_id", using: :btree
+    t.index ["user_id"], name: "index_moves_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,4 +67,5 @@ ActiveRecord::Schema.define(version: 20161015173334) do
   add_foreign_key "games_users", "games"
   add_foreign_key "games_users", "users"
   add_foreign_key "moves", "games"
+  add_foreign_key "moves", "users"
 end

392048bc20de139c1c4fb6522dd02a29837e5044
Grant Tuttle <grant.tuttle@gmail.com>
2016-10-18 14:12:51 -0400

    purging seeds.rb

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-require "active_model"
-require "application_helper"
-
-conn = ActiveRecord::Base.connection()
-
-# default cpu user
-User.create email: "grant.tuttle@gmail.com", password: "1337gamez", is_cpu: true
